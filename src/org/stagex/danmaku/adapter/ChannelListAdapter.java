2cb621d7de9a730e6148b8df355d8a025badbc0d
juguofeng <jgfntu@gmail.com>
2013-11-03 22:37:27 +0800

    播放界面切台列表增加节目预告

--- src/org/stagex/danmaku/adapter/ChannelListAdapter.java
+++ src/org/stagex/danmaku/adapter/ChannelListAdapter.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.keke.player.R;
 
+import com.fedorvlasov.lazylist2.ProgramLoader;
 import com.nmbb.oplayer.scanner.POChannelList;
 
 import android.annotation.SuppressLint;
@@ -27,10 +28,13 @@ public class ChannelListAdapter extends BaseAdapter {
 	
 	public int mCurrentIndex = -1;
 
+	public ProgramLoader programLoader;
+	
 	public ChannelListAdapter(Context context, List<POChannelList> infos) {
 		this.infos = infos;
 		this.mContext = context;
 		mLayoutInflater = LayoutInflater.from(context);
+		programLoader = new ProgramLoader(context);
 	}
 
 	@Override
@@ -65,6 +69,8 @@ public class ChannelListAdapter extends BaseAdapter {
 					.findViewById(R.id.channel_name);
 			viewHolder.textIndex = (TextView) convertView
 					.findViewById(R.id.channel_index);
+			viewHolder.pgmtext = (TextView) convertView
+					.findViewById(R.id.program_name);
 			viewHolder.hotView = (ImageView) convertView
 					.findViewById(R.id.hot_icon);
 			viewHolder.newView = (ImageView) convertView
@@ -77,7 +83,7 @@ public class ChannelListAdapter extends BaseAdapter {
 		// FIXME 2013-10-26 由于会清除infos的内存，故要加以保护
 		if (position < infos.size()) {
 			viewHolder.textName.setText(infos.get(position).name);
-			viewHolder.textIndex.setText(Integer.toString(position + 1));
+			viewHolder.textIndex.setText(Integer.toString(position + 1) + ".");
 		} else {
 //			Log.w("arrayOut", "=========arraryOut============");
 		}
@@ -88,23 +94,34 @@ public class ChannelListAdapter extends BaseAdapter {
 			// FIXME 2013-10-22 好像只能用Color.YELLOW，否则颜色不对
 			viewHolder.textName.setTextColor(Color.YELLOW);
 			viewHolder.textIndex.setTextColor(Color.YELLOW);
+			viewHolder.pgmtext.setTextColor(Color.YELLOW);
 		} else {
 			viewHolder.textName.setTextColor(Color.WHITE);
 			viewHolder.textIndex.setTextColor(Color.WHITE);
+			viewHolder.pgmtext.setTextColor(Color.GRAY);
 		}
 
 		// FIXME 2013-10-26 由于会清除infos的内存，故要加以保护
 		if (position < infos.size()) {
 			// 判断是否是热门频道，暂时使用HOT字样
-			if (infos.get(position).mode.equalsIgnoreCase("HOT"))
-				viewHolder.hotView.setVisibility(View.VISIBLE);
-			else
-				viewHolder.hotView.setVisibility(View.GONE);
-			// 判断是否是新频道，暂时用NEW字样
-			if (infos.get(position).mode.equalsIgnoreCase("NEW"))
-				viewHolder.newView.setVisibility(View.VISIBLE);
-			else
-				viewHolder.newView.setVisibility(View.GONE);
+//			if (infos.get(position).mode.equalsIgnoreCase("HOT"))
+//				viewHolder.hotView.setVisibility(View.VISIBLE);
+//			else
+//				viewHolder.hotView.setVisibility(View.GONE);
+//			// 判断是否是新频道，暂时用NEW字样
+//			if (infos.get(position).mode.equalsIgnoreCase("NEW"))
+//				viewHolder.newView.setVisibility(View.VISIBLE);
+//			else
+//				viewHolder.newView.setVisibility(View.GONE);
+			
+			// TODO 2013-10-17 实现节目预告功能
+			if (infos.get(position).program_path != null) {
+				programLoader.DisplayText(infos.get(position).program_path, null,
+						viewHolder.pgmtext);
+//				Log.w("arrayOut", "=====================");
+			} else {
+				viewHolder.pgmtext.setText("");
+			}
 		} else {
 //			Log.w("arrayOut", "=========arraryOut============");
 		}
@@ -115,6 +132,7 @@ public class ChannelListAdapter extends BaseAdapter {
 	private class ViewHolder {
 		TextView textName;
 		TextView textIndex;
+		TextView pgmtext;
 		ImageView hotView;
 		ImageView newView;
 	}
